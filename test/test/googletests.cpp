b8dbf4db82c936d9b185c1b4a53afce83aea5d11
Cléa Kunz <clea.kunz@epfl.ch>
2017-11-11 14:31:14 +0100

    Making of CMakeLists.txt, Doxyfile as well as googletests

---
+++ test/test/googletests.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Generation.hpp"
+#include "Simulation.hpp"
+#include "Allele.hpp"
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv) 
+{
+		::testing::InitGoogleTest(&argc, argv);
+		return RUN_ALL_TESTS();
+}
