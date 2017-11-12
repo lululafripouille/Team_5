05ed2c76c12f62bacb10b6a8d56d8d6514484cc1
sh4876 <32450791+sh4876@users.noreply.github.com>
2017-11-12 19:09:34 +0100

    attributs & prototypes dans Allele.hpp

--- src/Allele.hpp
+++ src/Allele.hpp
@@ -1,5 +1,49 @@
-#pragma once
+#pragma once 
 
-class Allele{
+#ifndef ALLELE_H
+#define ALLELE_H
+
+#include <vector>
+#include <fstream> 
+
+typedef std::vector<char> DNAbases;
+
+
+/*! \class Allele
+ * 
+ * this class gathers the characteristics of each allele 
+ * retrieved from the data base
+ * 
+ */
+ 
+ 
+class Allele
+{
+	private: 
+	
+		DNAbases sequences_;
+		std::vector<double> frequences_; 
+		unsigned int numberOfAllele_;
+		
+		/** retrieves the DNA sequence of the alleles contained 
+		 * in the genotype-database and initialises the attribut sequences_
+		 *   
+		 * */ 
+		void readFromFile(ostream& Data);
+	
+	public: 
+	
+		/** \fn allele constructor 
+		 *
+		 * */ 
+		Allele(); 
+		
+		/** allele destructor 
+		 *  
+		 * 
+		 * */  
+		 ~Allele(); 
 
 };
+
+#endif
